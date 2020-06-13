3df4b1764110bfde9b5d86fbe2566bb778d9f68c
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-13 12:52:01 +0530

    fetch data for currently reading

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -39,16 +39,13 @@ class ListBooks extends Component {
                       {/* <ListBooksOption /> */}
 
                       {this.state.currentlyReading.map( (book)=> 
-                      <li key={book.index}>
-                        {book}
-                      </li>     
-                       )}
-
-
-                      <li>
+                      
+                      <li key={book.id}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                                
+                            </div>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -59,10 +56,13 @@ class ListBooks extends Component {
                               </select>
                             </div>
                           </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
                         </div>
-                      </li>
+                      </li>    
+                       )}
+
+
                       
                     </ol>
                   </div>
