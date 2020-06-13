5a55255e0047b0c21e043e59b9696b37bec39ace
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-13 12:36:42 +0530

    add BooksApi in ListBooks component

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -1,9 +1,26 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
-import ListBooksOption from './ListBooksOption'
+import * as BooksAPI from './../BooksAPI';
 
 class ListBooks extends Component {
+
+    state = {
+        currentlyReading: [],
+        // wantToRead:[],
+        // read:[],
+    }
+  
+    componentDidMount() {
+      BooksAPI.getAll()
+       .then( (books)=> {
+           this.setState( ()=> ({
+              currentlyReading : books
+           }) )
+       } )
+    }
+
     render() {
+        console.log(this.state.currentlyReading)
   return (
             
         <div className="list-books">
@@ -11,18 +28,47 @@ class ListBooks extends Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
+
               <div>
+
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
+ 
+                      {/* <ListBooksOption /> */}
+
+                      {this.state.currentlyReading.map( (book)=> 
+                      <li key={book.index}>
+                        {book}
+                      </li>     
+                       )}
+
+
+                      <li>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">To Kill a Mockingbird</div>
+                          <div className="book-authors">Harper Lee</div>
+                        </div>
+                      </li>
                       
-                      <ListBooksOption />
-                     
                     </ol>
                   </div>
                 </div>
-                <div className="bookshelf">
+
+                {/* <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
@@ -65,6 +111,7 @@ class ListBooks extends Component {
                     </ol>
                   </div>
                 </div>
+
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
@@ -125,8 +172,10 @@ class ListBooks extends Component {
                       </li>
                     </ol>
                   </div>
-                </div>
+                </div> */}
+
               </div>
+
             </div>
             <div className="open-search">
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
