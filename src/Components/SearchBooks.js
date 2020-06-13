81b6e8a88ee6092c75fd83e3efc0c73e0dab084b
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-14 02:50:02 +0530

    finish serach bar

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -26,20 +26,45 @@ export default class SearchBooks extends Component {
 
     bookUpdateHandler(book, shelf) {
       BooksAPI.update(book, shelf)
-       .then( ()=> shelf !== 'none' ? alert(`${book.auther} add successfully`) : null )
+       .then( ()=> shelf !== 'none' ? alert(`${book.authors} add successfully`) : null )
        .catch( ()=> alert('Bad request') );
     }
    
 
+  searchBooksList() {
+    const { books, query } = this.state
+    if(query) {
+      return books.error ? 
+            <div> No Books Available </div>
+            : books.map( (book) => (
+                 <li key={book.id} >
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                              
+                          </div>
+                          <div className="book-shelf-changer">
+                            <select onChange={ (e)=> this.bookUpdateHandler(book, e.target.value) } value={book.shelf}>
+                              <option value="move">Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>   
+               ) 
+              )
+            
+    }
+  }
+
     render() {
 
-       const { books, query } = this.state
-      //  const showingBooks = query === '' ? books
-      //                           : books.filter( (c)=>(
-      //                               c.title.toLowerCase().includes(query.toLowerCase())
-      //                           ) )
-       console.log(books)
-       //console.log(query);
         return (
             <div className="search-books">
             <div className="search-books-bar">
@@ -59,38 +84,14 @@ export default class SearchBooks extends Component {
                 <input 
                    type="text" 
                    placeholder="Search by title or author"
-                   value={query} 
+                   value={this.state.query} 
                    onChange={ (e) => this.updateSearchHandler(e.target.value) } />
 
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-              
-              {   <li 
-                    key={book.id}
-                     >
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
-                                
-                            </div>
-                            <div className="book-shelf-changer">
-                              <select onChange={ (e)=> this.shelfHandler(book, e.target.value) } value={book.shelf}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                        </div>
-                      </li>    
-                  }
-
+               { this.searchBooksList() }
               </ol>
             </div>
           </div>
