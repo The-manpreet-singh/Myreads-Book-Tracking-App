7764fefda1c3d7f7d1468da931c604afa8f9c1e7
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-17 10:39:55 +0530

    convert into functional com

--- src/Components/ListBooksView.js
+++ src/Components/ListBooksView.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import PropTypes from 'prop-types';
 
-export default class ListBooksView extends Component {
-	static propTypes = {
-		clickShelfHandler: PropTypes.func.isRequired,
+function ListBooksView(props) {
+	const shelfUpdateHandler = (e) => {
+		props.clickShelfHandler(props.book, e.target.value);
 	};
 
-	shelfUpdateHandler = (e) => {
-		this.props.clickShelfHandler(this.props.book, e.target.value);
-	};
-	render() {
-		const img = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : null;
+	const img = props.book.imageLinks ? props.book.imageLinks.thumbnail : null;
 
-		return (
-			<div>
-				<li>
-					<div className='book'>
-						<div className='book-top'>
-							<div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${img})` }}></div>
-							<div className='book-shelf-changer'>
-								<select onChange={this.shelfUpdateHandler} value={this.props.book.shelf}>
-									<option value='move' disabled>
-										Move to...
-									</option>
-									<option value='currentlyReading'>Currently Reading</option>
-									<option value='wantToRead'>Want to Read</option>
-									<option value='read'>Read</option>
-									<option value='none'>None</option>
-								</select>
-							</div>
+	return (
+		<div>
+			<li>
+				<div className='book'>
+					<div className='book-top'>
+						<div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${img})` }}></div>
+						<div className='book-shelf-changer'>
+							<select onChange={shelfUpdateHandler} value={props.book.shelf}>
+								<option value='move' disabled>
+									Move to...
+								</option>
+								<option value='currentlyReading'>Currently Reading</option>
+								<option value='wantToRead'>Want to Read</option>
+								<option value='read'>Read</option>
+								<option value='none'>None</option>
+							</select>
 						</div>
-						<div className='book-title'>{this.props.book.title}</div>
-						<div className='book-authors'>{this.props.book.authors}</div>
 					</div>
-				</li>
-			</div>
-		);
-	}
+					<div className='book-title'>{props.book.title}</div>
+					<div className='book-authors'>{props.book.authors}</div>
+				</div>
+			</li>
+		</div>
+	);
 }
+
+ListBooksView.propTypes = {
+	clickShelfHandler: PropTypes.func.isRequired,
+};
+
+export default ListBooksView;
