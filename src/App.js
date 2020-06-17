b9e177ebbbd6601aba550704804c45620c800dc4
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-17 12:13:40 +0530

    remove commits

--- src/App.js
+++ src/App.js
@@ -12,34 +12,25 @@ import { Route } from 'react-router-dom';
 
 class BooksApp extends Component {
 	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
 		books: [],
 	};
 
 	componentDidMount() {
-		this.getAllBooks();
-	}
-
-	getAllBooks = () => {
 		BooksAPI.getAll().then((books) => {
 			this.setState({ books });
 		});
-	};
+	}
 
 	shelfHandler = (book, shelf) => {
-		BooksAPI.update(book, shelf).then(() => {
-			book.shelf = shelf;
-			this.setState((currentState) => ({
-				books: currentState.books.filter((c) => c.id !== book.id).concat(book),
-			}));
-		});
-		// .then(() => (shelf !== 'none' ? alert(`${book.authors} add successfully`) : null))
-		// .catch(() => alert('Bad request'));
+		BooksAPI.update(book, shelf)
+			.then(() => {
+				book.shelf = shelf;
+				this.setState((currentState) => ({
+					books: currentState.books.filter((c) => c.id !== book.id).concat(book),
+				}));
+			})
+			.then(() => (shelf !== 'none' ? alert(`${book.authors} add successfully`) : null))
+			.catch(() => alert('Bad request'));
 	};
 
 	render() {
